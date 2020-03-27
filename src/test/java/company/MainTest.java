06a56d6122127934ed41b3d33e6101eaf855baa7
pigoliver <43083270+pigoliver@users.noreply.github.com>
2020-03-27 22:39:38 +0800

    Update MainTest.java

--- src/test/java/company/MainTest.java
+++ src/test/java/company/MainTest.java
@@ -48,7 +48,7 @@ public class MainTest
 
         // read input and perform [Circular Shifting + Alphabetizing]
         Input.FileReader("TIC3001-Assignment1-testcases/testcase1/TitlesInput1.txt");
-        
+        System.setIn(sysInBackup);
 
 
         // Read Correct Expected Output into test
@@ -56,7 +56,7 @@ public class MainTest
 
         // assert output generated with Answer
         assertEquals(OutputResult, CorrectOutput);
-        System.setIn(sysInBackup);
+        
     }
 
     @Test
